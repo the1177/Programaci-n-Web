6e6fe0f6f37f188a0126b3a5de916d1f06fbbc1a
Jacob Elías <jacob1177a@gmail.com>
2018-02-10 23:17:11 -0600

    Arreglado reloj

--- script/script.js
+++ script/script.js
@@ -26,5 +26,5 @@ var myVar = setInterval(function() {
 
 function myTimer() {
   var d = new Date();
-  document.getElementById("clock").innerHTML = d.toLocaleTimeString();
+  document.getElementById("clock").innerHTML = "Hora actual<br>" + d.toLocaleTimeString();
 }
